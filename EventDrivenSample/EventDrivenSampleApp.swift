ebbbe87f40b4d81afc16562b16ba51e9b1bc15dc
Mitsuharu Emoto <mthr1982@gmail.com>
2024-03-24 14:05:08 +0900

    Initial Commit

---
+++ EventDrivenSample/EventDrivenSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventDrivenSampleApp.swift
+//  EventDrivenSample
+//
+//  Created by Mitsuharu Emoto on 2024/03/24.
+//
+
+import SwiftUI
+
+@main
+struct EventDrivenSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
